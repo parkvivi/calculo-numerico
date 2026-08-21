7665951b50fe9010798e64724244e0ccd019bc72
Viviane Park <viviflorp@gmail.com>
2026-08-21 18:14:38 -0300

    metodos.py

---
+++ metodos.py
@@ -0,0 +1,8 @@
+def bisseccao (f, a, b, eps=1e-8, max_iter=200):
+    # Retorna (raiz, historico)
+
+def newton (f, df, x0, eps=1e-8, max_iter=200):
+    # Retorna (raiz, historico)
+
+def secante (f, x0, x1, eps=1e-8, max_iter=200):
+    # Retorna (raiz, historico)
